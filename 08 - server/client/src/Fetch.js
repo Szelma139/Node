096f1c5342645c319a7e8a853deabfcb39f989ac
Szelma139 <raduniek1@gmail.com>
2021-01-03 17:18:46 +0100

    fetching user date

--- 08 - server/client/src/Fetch.js
+++ 08 - server/client/src/Fetch.js
@@ -1,4 +1,77 @@
 import React from "react";
+import styled from "styled-components";
+
+const TableContainer = styled.div`
+  margin: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StyledTr = styled.tr`
+  background-color: #009879;
+  color: #ffffff;
+  text-align: left;
+
+
+  td,
+  th {
+    padding: 12px 15px;
+    text-align: left;
+    margin-left: 1px solid #009811;
+    margin-right: 1px solid yellow;
+    
+  }
+`;
+
+const StyledButton = styled.button`
+padding:1rem 2rem 1rem 2rem;
+border: none;
+background-color: #6c7ae0;
+color: white;
+margin: auto;
+
+:hover {
+border-radius: 5px;
+background-color: green;
+border: none;
+text-decoration:underline; 
+}
+
+`;
+
+const StyledTable = styled.table`
+  border-collapse: collapse;
+  margin: 25px 0;
+  font-size: 1.5em;
+  font-family: sans-serif;
+  min-width: 800px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+
+  thead tr {
+    background-color: #36304a;;
+    color: white;
+    padding: 15px;
+  }
+  thead tr th:nth-child(0){
+    border-top-left-radius: 12px;
+    padding:10px;
+
+  }
+  thead tr th:last-child {
+    border-top-right-radius: 12px;
+    padding:12px;  }
+
+  ${StyledTr} {
+    background-color: white;
+    color: gray;
+  }
+
+  ${StyledTr}:nth-of-type(even) {
+    background-color: #f8f6ff;
+    color: gray;
+  }
+`;
 
 export const Fetch = () => {
   const [content, setContent] = React.useState([]);
@@ -12,15 +85,31 @@ export const Fetch = () => {
     // setContent(data)});
   }, []);
   return (
-    <div>
-      {content.map((element, index) => (
-        <div key={index}>
-        <p>{element.name}</p>
-        <p>{element.email}</p>
-        <p>{element.password}</p>
-        </div>
-      ))}
-    </div>
+    <TableContainer>
+      <StyledTable>
+        <thead>
+          <tr>
+            <th>Nr.</th>
+            <th>Username</th>
+            <th>E-mail</th>
+            <th>Password</th>
+            <th>Akcja</th>
+          </tr>
+        </thead>
+        <tbody>
+          {content.map((element, index) => (
+            <StyledTr key={index}>
+              <td>{index}</td>
+              <td>{element.name}</td>
+              <td>{element.email}</td>
+              <td>{element.password}</td>
+              <td><StyledButton>Edytuj</StyledButton></td>
+
+            </StyledTr>
+          ))}
+        </tbody>
+      </StyledTable>
+    </TableContainer>
   );
 };
 
