8bb3b3b64c04f5e76b9fb6144f48bbe2c6ca4916
Szelma139 <raduniek1@gmail.com>
2021-01-03 13:33:50 +0100

    connecting server and client together
    cors
    
    simple fetch test

--- 08 - server/routes/user.js
+++ 08 - server/routes/user.js
@@ -29,6 +29,7 @@ router.post("/registerUser", jsonParser, async (req, res) => {
     const selectQuery = "SELECT * from users where email = $1";
     const result = await pool.query(selectQuery, [email]);
 
+
     if (result.rows.length == 0 || result.rows.length === 0) {
       const insertQuery =
         "INSERT INTO users(name, email, password) VALUES ($1, $2, $3)";
@@ -37,7 +38,7 @@ router.post("/registerUser", jsonParser, async (req, res) => {
       return res.status(200).send("<h1>ok</h1>");
     }
     else {
-      return res.status(404).send("<h1>Already added Select</h1>")
+      return res.status(404).send("<h1>Already added </h1>")
     }
   } catch (error) {
     console.log(error);
